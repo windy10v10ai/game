30d71821457eec266a0b11b1896dbbdba5d5666c
windy <windybirth@gmail.com>
2024-11-15 21:48:04 +0900

    fix start command

--- src/scripts/launch.js
+++ src/scripts/launch.js
@@ -8,8 +8,22 @@ const { getAddonName, getDotaPath } = require("./utils");
 
   // You can add any arguments there
   // For example `+dota_launch_custom_game ${getAddonName()} dota` would automatically load "dota" map
-  const args = ["-novid", "-tools", "-addon", getAddonName()];
-  spawn(path.join(win64, "dota2.exe"), args, { detached: true, cwd: win64 });
+  const args = [
+    "-novid",
+    "-tools",
+    "-addon",
+    getAddonName(),
+    "+dota_launch_custom_game",
+    getAddonName(),
+    "dota",
+  ];
+
+  const child = spawn(path.join(win64, "dota2.exe"), args, {
+    detached: true,
+    cwd: win64,
+    stdio: "ignore", // 忽略子进程的输出
+  });
+  child.unref(); // 使父进程不等待子进程退出
 })().catch((error) => {
   console.error(error);
   process.exit(1);
