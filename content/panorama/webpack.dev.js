cac2b160810344ccc173fdc174306fdeee72655b
windy <windybirth@gmail.com>
2024-11-17 11:25:13 +0900

    修正indent和引号设置

--- content/panorama/webpack.dev.js
+++ content/panorama/webpack.dev.js
@@ -5,116 +5,119 @@ const { WatchIgnorePlugin } = require('webpack');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    mode: 'development',
-    context: path.resolve(__dirname, 'src'),
-    output: {
-        path: path.resolve(__dirname, 'layout/custom_game'),
-        publicPath: 'file://{resources}/layout/custom_game/',
-        chunkFormat: 'commonjs',
-    },
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'layout/custom_game'),
+    publicPath: 'file://{resources}/layout/custom_game/',
+    chunkFormat: 'commonjs',
+  },
 
-    watchOptions: {
-        aggregateTimeout: 1000, // 在1s内保存的所有文件都会被一次打包，因此也意味着每次按保存后要等一秒才能看到运行结果
-    },
+  watchOptions: {
+    aggregateTimeout: 1000, // 在1s内保存的所有文件都会被一次打包，因此也意味着每次按保存后要等一秒才能看到运行结果
+  },
 
-    optimization: {
-        usedExports: true, // 启用 tree shaking
-    },
+  optimization: {
+    usedExports: true, // 启用 tree shaking
+  },
 
-    cache: {
-        type: 'filesystem', // 降低首次运行的编译时间
-    },
+  cache: {
+    type: 'filesystem', // 降低首次运行的编译时间
+  },
 
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '...'],
-        symlinks: false,
-    },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '...'],
+    symlinks: false,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.xml$/,
-                loader: 'webpack-panorama-x/lib/layout-loader',
-                options: {},
-            },
-            {
-                test: /\.[jt]sx$/,
-                issuer: /\.xml$/,
-                loader: 'webpack-panorama-x/lib/entry-loader',
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                options: { transpileOnly: true },
-            },
-            {
-                test: /\.js?$|\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: [/node_modules/],
-                options: { presets: ['@babel/preset-react', '@babel/preset-env'] },
-            },
-            {
-                test: /\.css$/,
-                test: /\.(css|less)$/,
-                issuer: /\.xml$/,
-                loader: 'file-loader',
-                options: { name: '[path][name].css', esModule: false },
-            },
-            {
-                test: /\.less$/,
-                loader: 'less-loader',
-                options: {
-                    additionalData: content => {
-                        content = content.replace(/@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g, (match, name) => {
-                            // add apostrophe to satisfy valve
-                            return match.replace(name, `'${name}'`);
-                        });
-                        return content;
-                    },
-                    lessOptions: {
-                        relativeUrls: false,
-                    },
-                },
-            },
-        ],
-    },
-
-    plugins: [
-        new PanoramaTargetPlugin(),
-        new ForkTsCheckerWebpackPlugin({
-            typescript: {
-                configFile: path.resolve(__dirname, 'tsconfig.json'),
-            },
-        }),
-        new PanoramaManifestPlugin({
-            entries: [
-                // // js entires will be loaded in the scripts tag in the manifest
-                // { import: './utils/x-nettable-dispatcher.ts', filename: 'x-nettable-dispatcher.js' },
-
-                // // if type is not set, it will not be included in the manifest
-                // // usually used for loading screen, tooltips and popups which loaded
-                // // by engine or BLoadLayout etc.
-                // { import: './loading-screen/layout.xml', filename: 'custom_loading_screen.xml' },
-
-                // // provide type and filename to include in the manifest
-                // { import: './end_screen/layout.xml', type: 'EndScreen', filename: 'end_screen.xml' },
-
-                // if filename is not set, it will use the name of the entry
-                // { import: './team_select/team_select.xml', type: 'GameSetup' },
-                // HeroSelection
-                // { import: './hero_select/board/multiteam_hero_select_overlay.xml', type: 'HeroSelection' },
-                // FlyoutScoreboard
-                // { import: './hero_select/board/multiteam_hero_select_overlay.xml', type: 'HeroSelection' },
-                // { import: './board/boards.xml', type: 'Hud' },
-                // { import: './button/button.xml', type: 'Hud' },
-                // { import: './item_choice/item_choice.xml', type: 'Hud' },
-                // { import: './battlepass/battlepass.xml', type: 'Hud' },
-                // { import: './point_info/point_info.xml', type: 'Hud' },
-                { import: './hud/layout.xml', type: 'Hud' },
-                // { import: './end_screen_2/end_screen_2.xml', type: 'EndScreen' },
-            ],
-        }),
-        // use ignore plugin to ignore less files changes
-        new WatchIgnorePlugin({ paths: [/\.less$/] }),
+  module: {
+    rules: [
+      {
+        test: /\.xml$/,
+        loader: 'webpack-panorama-x/lib/layout-loader',
+        options: {},
+      },
+      {
+        test: /\.[jt]sx$/,
+        issuer: /\.xml$/,
+        loader: 'webpack-panorama-x/lib/entry-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true },
+      },
+      {
+        test: /\.js?$|\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        options: { presets: ['@babel/preset-react', '@babel/preset-env'] },
+      },
+      {
+        test: /\.css$/,
+        test: /\.(css|less)$/,
+        issuer: /\.xml$/,
+        loader: 'file-loader',
+        options: { name: '[path][name].css', esModule: false },
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader',
+        options: {
+          additionalData: (content) => {
+            content = content.replace(
+              /@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g,
+              (match, name) => {
+                // add apostrophe to satisfy valve
+                return match.replace(name, `'${name}'`);
+              },
+            );
+            return content;
+          },
+          lessOptions: {
+            relativeUrls: false,
+          },
+        },
+      },
     ],
+  },
+
+  plugins: [
+    new PanoramaTargetPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        configFile: path.resolve(__dirname, 'tsconfig.json'),
+      },
+    }),
+    new PanoramaManifestPlugin({
+      entries: [
+        // // js entires will be loaded in the scripts tag in the manifest
+        // { import: './utils/x-nettable-dispatcher.ts', filename: 'x-nettable-dispatcher.js' },
+
+        // // if type is not set, it will not be included in the manifest
+        // // usually used for loading screen, tooltips and popups which loaded
+        // // by engine or BLoadLayout etc.
+        // { import: './loading-screen/layout.xml', filename: 'custom_loading_screen.xml' },
+
+        // // provide type and filename to include in the manifest
+        // { import: './end_screen/layout.xml', type: 'EndScreen', filename: 'end_screen.xml' },
+
+        // if filename is not set, it will use the name of the entry
+        // { import: './team_select/team_select.xml', type: 'GameSetup' },
+        // HeroSelection
+        // { import: './hero_select/board/multiteam_hero_select_overlay.xml', type: 'HeroSelection' },
+        // FlyoutScoreboard
+        // { import: './hero_select/board/multiteam_hero_select_overlay.xml', type: 'HeroSelection' },
+        // { import: './board/boards.xml', type: 'Hud' },
+        // { import: './button/button.xml', type: 'Hud' },
+        // { import: './item_choice/item_choice.xml', type: 'Hud' },
+        // { import: './battlepass/battlepass.xml', type: 'Hud' },
+        // { import: './point_info/point_info.xml', type: 'Hud' },
+        { import: './hud/layout.xml', type: 'Hud' },
+        // { import: './end_screen_2/end_screen_2.xml', type: 'EndScreen' },
+      ],
+    }),
+    // use ignore plugin to ignore less files changes
+    new WatchIgnorePlugin({ paths: [/\.less$/] }),
+  ],
 };
