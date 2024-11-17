8ed0409b3eb7be4508935f52034072bf92dcf198
mimihua <68930310+mimihua@users.noreply.github.com>
2024-11-17 22:15:51 +0900

    Implement panorama using React #1086 (#1092)
    * 使用react实装paranoma画面 #1086
    
    * webpack
    
    * 修正indent和引号设置
    
    * Update .eslintrc.json
    
    * move path
    
    * remove
    
    * out put to react folder
    
    * Update package.json
    
    * Update custom_ui_manifest.xml
    
    ---------
    
    Co-authored-by: windy <windybirth@gmail.com>
    Former-commit-id: 8ec3f3631ef500f9e20a8ee7782e11ca05b93a92

---
+++ src/panorama/webpack.dev.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { PanoramaTargetPlugin } = require('webpack-panorama-x');
+const { WatchIgnorePlugin } = require('webpack');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: {
+    hud: { filename: 'hud/layout.xml', import: './hud/layout.xml' },
+  },
+  mode: 'development',
+  context: path.resolve(__dirname, 'react'),
+  output: {
+    path: path.resolve(__dirname, '../../content/panorama/layout/custom_game/react/'),
+    publicPath: 'file://{resources}/layout/custom_game/react/',
+    chunkFormat: 'commonjs',
+  },
+
+  watchOptions: {
+    aggregateTimeout: 1000, // 在1s内保存的所有文件都会被一次打包，因此也意味着每次按保存后要等一秒才能看到运行结果
+  },
+
+  optimization: {
+    usedExports: true, // 启用 tree shaking
+  },
+
+  cache: {
+    type: 'filesystem', // 降低首次运行的编译时间
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '...'],
+    symlinks: false,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.xml$/,
+        loader: 'webpack-panorama-x/lib/layout-loader',
+        options: {},
+      },
+      {
+        test: /\.[jt]sx$/,
+        issuer: /\.xml$/,
+        loader: 'webpack-panorama-x/lib/entry-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true },
+      },
+      {
+        test: /\.js?$|\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        options: { presets: ['@babel/preset-react', '@babel/preset-env'] },
+      },
+      {
+        test: /\.css$/,
+        test: /\.(css|less)$/,
+        issuer: /\.xml$/,
+        loader: 'file-loader',
+        options: { name: '[path][name].css', esModule: false },
+      },
+      {
+        test: /\.less$/,
+        loader: 'less-loader',
+        options: {
+          additionalData: (content) => {
+            content = content.replace(
+              /@keyframes\s*(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g,
+              (match, name) => {
+                // add apostrophe to satisfy valve
+                return match.replace(name, `'${name}'`);
+              },
+            );
+            return content;
+          },
+          lessOptions: {
+            relativeUrls: false,
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new PanoramaTargetPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        configFile: path.resolve(__dirname, 'tsconfig.json'),
+      },
+    }),
+    // use ignore plugin to ignore less files changes
+    new WatchIgnorePlugin({ paths: [/\.less$/] }),
+  ],
+};
