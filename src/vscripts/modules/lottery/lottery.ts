1ea15a5f4f4a8a68221efc25938fed467e7c8c7f
windy <windybirth@gmail.com>
2024-11-28 14:34:58 +0900

    refactor

--- src/vscripts/modules/lottery/lottery.ts
+++ src/vscripts/modules/lottery/lottery.ts
@@ -1,14 +1,13 @@
 import { LotteryDto } from '../../../common/dto/lottery';
 import { PlayerHelper } from '../../helper/player-helper';
 import { reloadable } from '../../utils/tstl-utils';
-
-interface Tier {
-  rate: number;
-  names: string[];
-}
+import { itemTier, Tier } from './tier-data';
 
 @reloadable
 export class Lottery {
+  readonly normalItemCount = 3;
+  readonly memberItemCount = 1;
+
   constructor() {
     // 启动物品抽奖
     ListenToGameEvent(
@@ -36,10 +35,6 @@ export class Lottery {
     CustomGameEventManager.RegisterListener('lottery_refresh_item', (userId, event) => {
       this.refreshItem(userId, event);
     });
-
-    this.itemTier.sort((a, b) => {
-      return b.rate - a.rate;
-    });
   }
 
   StartItemLottery() {
@@ -52,107 +47,12 @@ export class Lottery {
     });
   }
 
-  readonly normalItemCount = 3;
-  readonly memberItemCount = 1;
-  /**
-   * 物品抽选概率，Tier 1-5
-   */
-  readonly itemTier: Tier[] = [
-    {
-      rate: 100,
-      names: [
-        'item_bottle', // 魔瓶
-        'item_bracer', // 护腕
-        'item_wraith_band', // 系带
-        'item_null_talisman', // 挂件
-        'item_infused_raindrop', // 凝魂之露
-
-        // ---- 中立物品 lv1 ----
-        'item_chipped_vest', // 碎裂背心
-        'item_ironwood_tree', // 铁树之木
-        'item_iron_talon', // 打野爪
-        'item_keen_optic', // 基恩镜片
-        'item_possessed_mask', // 附魂面具
-        'item_ring_of_aquila', // 天鹰戒
-        'item_poor_mans_shield', // 穷鬼盾
-        'item_broom_handle', // 扫帚柄
-      ],
-    },
-    {
-      rate: 40,
-      names: [
-        'item_hand_of_midas', // 点金手
-        'item_holy_locket', // 圣洁吊坠
-        'item_aghanims_shard', // 阿哈利姆魔晶
-        'item_great_famango', // 大疗伤莲花
-        'item_tome_of_knowledge', // 知识之书
-
-        // ---- 中立物品 lv2 ----
-        'item_imp_claw', // 魔童之爪
-        'item_vampire_fangs', // 吸血鬼獠牙
-        'item_mysterious_hat', // 仙灵饰品
-        'item_vambrace', // 臂甲
-        'item_grove_bow', // 林野长弓
-        'item_orb_of_destruction', // 毁灭灵球
-        'item_philosophers_stone', // 贤者石
-        'item_essence_ring', // 精华指环
-      ],
-    },
-    {
-      rate: 12,
-      names: [
-        // ---- 中立物品 lv3 ----
-        'item_titan_sliver', // 巨神残铁
-        'item_quickening_charm', // 加速护符
-        'item_spider_legs', // 网虫腿
-        'item_horizon', // 视界
-        'item_witless_shako', // 无知小帽
-        'item_third_eye', // 第三只眼
-        'item_the_leveller', // 平世剑
-        'item_paladin_sword', // 骑士剑
-      ],
-    },
-    {
-      rate: 2,
-      names: [
-        'item_light_part', // 圣光组件
-        'item_dark_part', // 暗影组件
-
-        // ---- 中立物品 lv4 ----
-        'item_penta_edged_sword', // 五锋长剑
-        'item_panic_button', // 神灯
-        'item_minotaur_horn', // 恶牛角
-        'item_spell_prism', // 法术棱镜
-        'item_helm_of_the_undying', // 不死头盔
-        'item_woodland_striders', // 丛林鞋
-        'item_princes_knife', // 亲王短刀
-        'item_repair_kit', // 维修器具
-      ],
-    },
-    {
-      rate: 0.5,
-      names: [
-        'item_tome_of_agility', // 敏捷之书
-        'item_tome_of_intelligence', // 智力之书
-        'item_tome_of_strength', // 力量之书
-
-        // ---- 中立物品 lv5 ----
-        'item_fallen_sky', // 天崩
-        'item_desolator_2', // 寂灭
-        'item_mirror_shield', // 神镜盾
-        'item_ballista', // 弩炮
-        'item_seer_stone', // 先哲石
-        'item_ex_machina', // 机械之心
-        'item_pirate_hat', // 海盗帽
-      ],
-    },
-  ];
-
   // 随机决定tier
   private getRandomTier(tiers: Tier[]): Tier {
     const draw = Math.random() * 100;
     // 获取level最高，draw小于rate的tier
     print(`Draw rate ${draw}`);
+    DeepPrintTable(tiers);
 
     for (const tier of tiers) {
       if (draw <= tier.rate) {
@@ -187,7 +87,7 @@ export class Lottery {
   RandomItemForPlayer(playerId: PlayerID) {
     // 抽取3个普通物品和1个会员物品，如果有重复的，重新抽取
     const items = this.getRandomNames(
-      this.itemTier,
+      itemTier,
       this.normalItemCount + this.memberItemCount,
       'item_branches',
     );
@@ -235,11 +135,27 @@ export class Lottery {
 
   // 刷新物品
   refreshItem(userId: EntityIndex, event: LotteryRefreshItemEventData) {
+    print('Refresh item');
     const steamAccountId = PlayerResource.GetSteamAccountID(event.PlayerID);
-    const lotteryDto = CustomNetTables.GetTableValue(
-      'lottery',
-      steamAccountId.toString(),
-    ) as LotteryDto;
+    const lotteryRaw = CustomNetTables.GetTableValue('lottery', steamAccountId.toString());
+
+    print('lotteryRaw');
+    DeepPrintTable(lotteryRaw);
+    const lotteryDto: LotteryDto = Object.assign(
+      {
+        itemNamesNormal: [],
+        itemNamesMember: [],
+        pickItemName: undefined,
+      },
+      lotteryRaw,
+    );
+
+    print('lotteryDto');
+    DeepPrintTable(lotteryDto);
+    lotteryDto.itemNamesNormal.push(lotteryDto.itemNamesMember[0]);
+
+    print('lotteryDto add');
+    DeepPrintTable(lotteryDto);
 
     if (lotteryDto.pickItemName) {
       return;
