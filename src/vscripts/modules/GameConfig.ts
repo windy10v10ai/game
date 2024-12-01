f96a65340acfb552bd709e2f9a646969f99625c4
windy <windybirth@gmail.com>
2024-12-01 23:25:43 +0900

    Strengthen N6 bot (#1138)
    * add text
    
    * Create bot_power.png
    
    * add ability
    
    * fix
    
    * impl
    
    * Update lottery-abilities.ts
    
    * Update GameConfig.ts
    
    * Update npc_abilities_custom.txt

--- src/vscripts/modules/GameConfig.ts
+++ src/vscripts/modules/GameConfig.ts
@@ -60,10 +60,10 @@ export class GameConfig {
 
     if (IsInToolsMode()) {
       print('[GameConfig] IsInToolsMode set');
-      GameRules.SetCustomGameSetupAutoLaunchDelay(1);
+      // GameRules.SetCustomGameSetupAutoLaunchDelay(1);
       GameRules.SetHeroSelectionTime(30);
       GameRules.SetHeroSelectPenaltyTime(1); // 选择英雄超时惩罚时间
-      GameRules.SetStrategyTime(1);
+      GameRules.SetStrategyTime(5);
       // GameRules.SetPreGameTime(300);
     }
   }
