59a506d04b8e1c43921415e839e0ba1a307fced2
windy <windybirth@gmail.com>
2024-12-10 22:34:20 +0900

    Refactor tab scoreboard. Remove lua is member (#1161)
    * Update npc_items_custom.txt
    
    * Update npc_abilities_override.txt
    
    * refactor member buy back cd
    
    * refactor
    
    Former-commit-id: aeb1b1a9728372072b8f01e1d5d78f873c2739db

--- src/vscripts/modules/GameConfig.ts
+++ src/vscripts/modules/GameConfig.ts
@@ -1,5 +1,6 @@
 export class GameConfig {
   public static readonly GAME_VERSION = 'v4.03';
+  public static readonly MEMBER_BUYBACK_CD = 120;
   constructor() {
     SendToServerConsole('dota_max_physical_items_purchase_limit 9999'); // 用来解决物品数量限制问题
 
@@ -61,11 +62,10 @@ export class GameConfig {
 
     if (IsInToolsMode()) {
       print('[GameConfig] IsInToolsMode set');
-      GameRules.SetCustomGameSetupAutoLaunchDelay(1);
+      // GameRules.SetCustomGameSetupAutoLaunchDelay(1);
       GameRules.SetHeroSelectionTime(15);
       GameRules.SetHeroSelectPenaltyTime(1); // 选择英雄超时惩罚时间
       GameRules.SetStrategyTime(5);
-      // GameRules.SetPreGameTime(300);
     }
   }
 }
