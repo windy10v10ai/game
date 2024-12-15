e0e73fe44d915e0af6c6450cac23fc865a2bc5e7
windy <windybirth@gmail.com>
2024-12-15 22:35:18 +0900

    Update lottery-abilities.ts

--- src/vscripts/modules/lottery/lottery-abilities.ts
+++ src/vscripts/modules/lottery/lottery-abilities.ts
@@ -15,7 +15,6 @@ export const abilityTiers: Tier[] = [
       // 被动技能
       'omniknight_hammer_of_purity', // 纯洁之锤
       'elder_titan_natural_order', // 自然秩序
-      'bristleback_bristleback', // 钢毛后背
       'spectre_dispersion', // 折射
       'drow_ranger_marksmanship', // 射手天赋
     ],
@@ -26,14 +25,12 @@ export const abilityTiers: Tier[] = [
     names: [
       // 法球/开关技能
       'doom_bringer_infernal_blade', // 阎刃
-      'obsidian_destroyer_arcane_orb', // 奥术天球
       'enchantress_impetus', // 推进
 
       // 被动技能
-      'ogre_magi_multicast', // 多重施法
+      'bristleback_bristleback', // 钢毛后背
       'phantom_assassin_coup_de_grace', // 恩赐解脱
       'templar_assassin_psi_blades', // 灵能之刃
-      'troll_warlord_fervor', // 热血战魂
       'dazzle_good_juju', // 善咒
       'rubick_arcane_supremacy', // 奥术至尊
       'sven_great_cleave', // 巨力挥舞
@@ -48,12 +45,14 @@ export const abilityTiers: Tier[] = [
     rate: 20,
     names: [
       // 法球/开关技能
-      'viper_poison_attack', // 毒性攻击
+      'obsidian_destroyer_arcane_orb', // 奥术天球
       'silencer_glaives_of_wisdom', // 智慧之刃
       'ancient_apparition_chilling_touch', // 极寒之触
       'gyrocopter_flak_cannon', // 高射火炮
 
       // 被动技能
+      'troll_warlord_fervor', // 热血战魂
+      'ogre_magi_multicast', // 多重施法
       'huskar_berserkers_blood', // 狂战士之血
       'slark_essence_shift', // 能量转移
       'chaos_knight_chaos_strike', // 混沌一击
@@ -78,8 +77,7 @@ export const abilityTiers: Tier[] = [
     names: [
       // 法球/开关技能
       'drow_ranger_frost_arrows', // 霜冻之箭
-      'jakiro_liquid_fire', // 液态火
-      'jakiro_liquid_ice', // 液态冰
+      'viper_poison_attack', // 毒性攻击
 
       // 被动技能
       'tiny_grow', // 长大
@@ -93,13 +91,13 @@ export const abilityTiers: Tier[] = [
       'juggernaut_blade_dance', // 剑舞
       'skeleton_king_mortal_strike', // 本命一击
       'night_stalker_hunter_in_the_night', // 暗夜猎影
-      'alchemist_corrosive_weaponry', // 腐蚀兵械
       'sniper_headshot', // 爆头
       'nevermore_dark_lord', // 魔王降临
       'dawnbreaker_luminosity', // 熠熠生辉
       'lina_fiery_soul', // 炽魂
       'mars_bulwark', // 护身甲盾
       'lycan_feral_impulse', // 野性驱使
+      'weaver_geminate_attack', // 连击
     ],
   },
   {
@@ -107,22 +105,24 @@ export const abilityTiers: Tier[] = [
     rate: 100,
     names: [
       // 法球/开关技能
+      'jakiro_liquid_fire', // 液态火
+      'jakiro_liquid_ice', // 液态冰
       'witch_doctor_voodoo_restoration', // 巫毒疗法
 
       // 被动技能
+      'alchemist_corrosive_weaponry', // 腐蚀兵械
       'visage_gravekeepers_cloak', // 陵卫斗篷
       'storm_spirit_overload', // 超负荷
       'life_stealer_ghoul_frenzy', // 尸鬼狂怒
-      'weaver_geminate_attack', // 连击
       // 'venomancer_poison_sting', // 剧毒术士 毒刺
       'broodmother_incapacitating_bite', // 麻痹之咬
       'shredder_reactive_armor', // 活性活甲
       'viper_corrosive_skin', // 腐蚀皮肤
       'beastmaster_inner_beast', // 野性之心
       'rubick_null_field', // 失效立场
-      // 'brewmaster_fire_phase', // 永久相位
+      'brewmaster_fire_phase', // 永久相位
       // 'brewmaster_fire_permanent_immolation', // 永久献祭
-      'silencer_last_word', // 遗言
+      // 'silencer_last_word', // 遗言
       'tidehunter_kraken_shell', // 海妖外壳
       'pangolier_lucky_shot', // 幸运一击
       'phantom_lancer_phantom_edge', // 幻影冲锋
