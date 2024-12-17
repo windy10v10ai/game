56a06e99a0457a11e43837687d6d0ad640dc41de
windy <windybirth@gmail.com>
2024-12-17 21:19:15 +0900

    remove swagger

--- src/vscripts/api/analytics/dto/event-base-dto.ts
+++ src/vscripts/api/analytics/dto/event-base-dto.ts
@@ -1,15 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class EventBaseDto {
-  @ApiProperty()
   matchId: string;
 
-  @ApiProperty({ default: 'v4.00' })
   version: string;
 
-  @ApiProperty({ default: 0 })
   difficulty: number;
 
-  @ApiProperty()
   steamId: number;
 }
--- src/vscripts/api/analytics/dto/pick-ability-dto.ts
+++ src/vscripts/api/analytics/dto/pick-ability-dto.ts
@@ -1,11 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 import { EventBaseDto } from './event-base-dto';
 
 export class PickDto extends EventBaseDto {
-  @ApiProperty({ default: 'name' })
   name: string;
 
-  @ApiProperty()
   level: number;
 }
