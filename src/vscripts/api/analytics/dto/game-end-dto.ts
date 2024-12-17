ad16fe6f5ffd65ba001f87de6c94a9346f7ea806
windy <windybirth@gmail.com>
2024-12-17 23:31:10 +0900

    Update game-end-dto.ts

--- src/vscripts/api/analytics/dto/game-end-dto.ts
+++ src/vscripts/api/analytics/dto/game-end-dto.ts
@@ -2,42 +2,34 @@ import { EventBaseDto } from './event-base-dto';
 
 export class GameEndGameOptionsDto {
   multiplierRadiant: number;
-
   multiplierDire: number;
-
   playerNumberRadiant: number;
-
   playerNumberDire: number;
-
   towerPowerPct: number;
 }
 
 export class GameEndPlayerDto {
   heroName: string;
-
   steamId: number;
-
   teamId: number;
-
   isDisconnected: boolean;
-
   level: number;
-
   gold: number;
-
   kills: number;
-
   deaths: number;
-
   assists: number;
-
   points: number;
+
+  // 追加项目
+  damage: number;
+  damageTaken: number;
+  lastHits: number;
+  healing: number;
+  towerKills: number;
 }
 
 export class GameEndDto extends EventBaseDto {
   gameOptions: GameEndGameOptionsDto;
-
   winnerTeamId: number;
-
   players: GameEndPlayerDto[];
 }
