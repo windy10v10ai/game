fef010fb7149e5c1e9570ce89d5710290f525b65
windy <windybirth@gmail.com>
2025-01-21 17:42:44 +0900

    fix

--- src/vscripts/modules/event/game-end/game-end-helper.test.ts
+++ src/vscripts/modules/event/game-end/game-end-helper.test.ts
@@ -72,7 +72,7 @@ describe('GameEndHelper', () => {
       };
 
       const score = GameEndHelper.CalculatePlayerScore(player);
-      expect(score).toBe(57);
+      expect(score).toBe(54);
     });
 
     it('should calculate score correctly for a player with extra high stats', () => {
@@ -96,7 +96,7 @@ describe('GameEndHelper', () => {
       };
 
       const score = GameEndHelper.CalculatePlayerScore(player);
-      expect(score).toBe(148);
+      expect(score).toBe(138);
     });
   });
 
--- src/vscripts/modules/event/game-end/game-end-helper.ts
+++ src/vscripts/modules/event/game-end/game-end-helper.ts
@@ -4,12 +4,12 @@ import { reloadable } from '../../../utils/tstl-utils';
 @reloadable
 export class GameEndHelper {
   static CalculatePlayerScore(player: GameEndPlayerDto): number {
-    const killScore = Math.sqrt(player.kills) * 1.1;
+    const killScore = Math.sqrt(player.kills) * 1.2;
     const deathScore = -Math.sqrt(player.deaths) * 0.5;
     const assistScore = Math.sqrt(player.assists) * 1.2;
-    const damageScore = Math.min(40, Math.sqrt(player.damage) / 160);
-    const damageTakenScore = Math.min(40, Math.sqrt(player.damageTaken) / 80);
-    const healingScore = Math.min(40, Math.sqrt(player.healing) / 40);
+    const damageScore = Math.min(40, Math.sqrt(player.damage) / 180);
+    const damageTakenScore = Math.min(40, Math.sqrt(player.damageTaken) / 90);
+    const healingScore = Math.min(40, Math.sqrt(player.healing) / 50);
     const towerKillScore = Math.sqrt(player.towerKills) * 3;
 
     const totalScore =
