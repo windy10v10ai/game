96038f8caf1ae11d5467a3484f5026adb08abe75
windy <windybirth@gmail.com>
2025-02-20 09:09:56 +0900

    Update Debug.ts

--- src/vscripts/modules/debug/Debug.ts
+++ src/vscripts/modules/debug/Debug.ts
@@ -90,8 +90,8 @@ export class Debug {
       const hero = PlayerResource.GetSelectedHeroEntity(keys.playerid);
       if (!hero) return;
       // 获得金钱经验技能升满
-      hero.SetGold(99999, false);
-      hero.AddExperience(100000, ModifyXpReason.UNSPECIFIED, false, true);
+      hero.ModifyGold(99999, false, ModifyGoldReason.UNSPECIFIED);
+      hero.AddExperience(99999, ModifyXpReason.UNSPECIFIED, false, true);
       // refresh teleport
       hero.GetItemInSlot(15)?.EndCooldown();
     }
@@ -102,8 +102,8 @@ export class Debug {
         const hero = PlayerResource.GetSelectedHeroEntity(playerId);
         if (!hero) return;
         // 获得金钱经验技能升满
-        hero.SetGold(40000, false);
-        hero.AddExperience(40000, ModifyXpReason.UNSPECIFIED, false, true);
+        hero.ModifyGold(50000, false, ModifyGoldReason.UNSPECIFIED);
+        hero.AddExperience(50000, ModifyXpReason.UNSPECIFIED, false, true);
       });
     }
 
