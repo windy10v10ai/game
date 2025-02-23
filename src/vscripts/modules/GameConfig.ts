057e3a1d2f8b3b459bb6509695086c6589192722
windy <windybirth@gmail.com>
2025-02-24 01:18:55 +0900

    game setting

--- src/vscripts/modules/GameConfig.ts
+++ src/vscripts/modules/GameConfig.ts
@@ -35,13 +35,13 @@ export class GameConfig {
     game.SetSelectionGoldPenaltyEnabled(true); // 是否启用选择英雄时的金钱惩罚（超时每秒扣钱）
     game.SetLoseGoldOnDeath(false); // 是否在英雄死亡时扣除金钱
     game.SetCustomBuybackCostEnabled(true); // 是否启用自定义买活价格
+    game.SetCustomGlyphCooldown(180); // 防御符文冷却时间
+
     // game.SetBuybackEnabled(false); // 是否允许买活
     // game.SetDaynightCycleDisabled(true); // 是否禁用白天黑夜循环
     // game.SetForceRightClickAttackDisabled(true); // 是否禁用右键攻击
     // game.SetHudCombatEventsDisabled(true); // 是否禁用战斗事件（左下角的战斗消息）
     // game.SetCustomGameForceHero(`npc_dota_hero_phoenix`); // 设置强制英雄（会直接跳过英雄选择阶段并直接为所有玩家选择这个英雄）
-
-    // game.SetDaynightCycleDisabled(true); // 是否禁用白天黑夜循环
     // game.SetDeathOverlayDisabled(true); // 是否禁用死亡遮罩（灰色的遮罩）
 
     // 每点智力魔抗加成在modifier intelect_magic_resist.ts中控制
@@ -58,7 +58,7 @@ export class GameConfig {
     if (IsInToolsMode()) {
       print('[GameConfig] IsInToolsMode set');
       GameRules.SetCustomGameSetupAutoLaunchDelay(3);
-      // GameRules.SetHeroSelectionTime(5);
+      GameRules.SetHeroSelectionTime(15);
       GameRules.SetHeroSelectPenaltyTime(1); // 选择英雄超时惩罚时间
       // GameRules.SetStrategyTime(5);
       // GameRules.SetPreGameTime(15); // 进入游戏后号角吹响前的准备时间
