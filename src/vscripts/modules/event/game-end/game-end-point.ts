343f355f07d86480cecb30e1f6fcc2a4915729d0
windy <windybirth@gmail.com>
2025-03-04 09:16:27 +0900

    Fix Skeleton King talent. Prevent multicast while hero is channeling (#1335)
    * Update team_select.js
    
    * Add Skeleton King spectral blade damage bonus and fix localization typos
    
    * Prevent multicast from triggering while the hero is channeling an ability
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update npc_abilities_override.txt

--- src/vscripts/modules/event/game-end/game-end-point.ts
+++ src/vscripts/modules/event/game-end/game-end-point.ts
@@ -5,12 +5,12 @@ import { reloadable } from '../../../utils/tstl-utils';
 export class GameEndPoint {
   static CalculatePlayerScore(player: GameEndPlayerDto): number {
     const killScore = Math.sqrt(player.kills) * 1.2;
-    const deathScore = -Math.sqrt(player.deaths) * 0.5;
-    const assistScore = Math.sqrt(player.assists) * 1.2;
+    const deathScore = -Math.sqrt(player.deaths) * 0.6;
+    const assistScore = Math.sqrt(player.assists) * 1.3;
     const damageScore = Math.min(40, Math.sqrt(player.damage) / 200);
     const damageTakenScore = Math.min(40, Math.sqrt(player.damageTaken) / 100);
     const healingScore = Math.min(40, Math.sqrt(player.healing) / 50);
-    const towerKillScore = Math.sqrt(player.towerKills) * 3;
+    const towerKillScore = Math.sqrt(player.towerKills) * 4;
 
     const totalScore =
       killScore +
@@ -26,7 +26,7 @@ export class GameEndPoint {
 
   static GetGameTimePoints(gameTime: number): number {
     const min = gameTime / 60;
-    const points = Math.sqrt(min) * 3.8;
+    const points = Math.sqrt(min) * 3.5;
     return Math.round(points);
   }
 }
