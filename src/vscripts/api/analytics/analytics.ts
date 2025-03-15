30d8db272c2e108d67a5e1f489d636491ff51549
windy <windybirth@gmail.com>
2025-03-15 16:16:39 +0900

    Stop sending lottery pick event (#1357)
    Former-commit-id: 4a5687a58fbf446d69f03fa4aa1cbe4508bd03fd

--- src/vscripts/api/analytics/analytics.ts
+++ src/vscripts/api/analytics/analytics.ts
@@ -4,21 +4,21 @@ import { PickDto } from './dto/pick-ability-dto';
 
 @reloadable
 export class Analytic {
-  public static readonly POST_LOTTERY_PICK_ABILITY_URL = '/analytics/lottery/pick/ability';
+  // public static readonly POST_LOTTERY_PICK_ABILITY_URL = '/analytics/lottery/pick/ability';
   public static readonly POST_GAME_END_PICK_ABILITY_URL = '/analytics/game-end/pick/ability';
 
-  public static async SendPickAbilityEvent(pickDto: PickDto) {
-    const apiParameter: ApiParameter = {
-      method: HttpMethod.POST,
-      path: this.POST_LOTTERY_PICK_ABILITY_URL,
-      body: pickDto,
-      successFunc: () => {
-        print(`[Analytic] sendPickAbilityEvent success`);
-      },
-    };
+  // public static async SendPickAbilityEvent(pickDto: PickDto) {
+  //   const apiParameter: ApiParameter = {
+  //     method: HttpMethod.POST,
+  //     path: this.POST_LOTTERY_PICK_ABILITY_URL,
+  //     body: pickDto,
+  //     successFunc: () => {
+  //       print(`[Analytic] sendPickAbilityEvent success`);
+  //     },
+  //   };
 
-    ApiClient.sendWithRetry(apiParameter);
-  }
+  //   ApiClient.sendWithRetry(apiParameter);
+  // }
 
   public static async SendGameEndPickAbilityEvent(pickDto: PickDto) {
     const apiParameter: ApiParameter = {
