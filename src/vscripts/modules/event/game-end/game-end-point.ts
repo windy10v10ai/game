9b07e75bd38d73192b2d8cf7caeb90a0f94d7756
windy <windybirth@gmail.com>
2025-04-05 22:42:30 +0900

    balance point

--- src/vscripts/modules/event/game-end/game-end-point.ts
+++ src/vscripts/modules/event/game-end/game-end-point.ts
@@ -4,7 +4,7 @@ import { reloadable } from '../../../utils/tstl-utils';
 @reloadable
 export class GameEndPoint {
   static CalculatePlayerScore(player: GameEndPlayerDto): number {
-    const killScore = Math.sqrt(player.kills) * 1.3;
+    const killScore = Math.sqrt(player.kills) * 1.4;
     const deathScore = -Math.sqrt(player.deaths) * 0.6;
     const assistScore = Math.sqrt(player.assists) * 1.4;
     const damageScore = Math.min(40, Math.sqrt(player.damage) / 200);
@@ -50,7 +50,7 @@ export class GameEndPoint {
 
   static GetGameTimePoints(gameTime: number): number {
     const min = gameTime / 60;
-    const points = Math.sqrt(min) * 3.2;
+    const points = Math.sqrt(min) * 3;
     return Math.round(points);
   }
 }
