883fd9973921c57b5d7795165a96c8e2eb78a82c
windy <windybirth@gmail.com>
2025-04-06 11:47:56 +0900

    Update hero-facet-config.ts

--- src/vscripts/modules/hero/hero-facet-config.ts
+++ src/vscripts/modules/hero/hero-facet-config.ts
@@ -1,11 +1,11 @@
 export class HeroFacetConfig {
   // 英雄名称到facetID列表的映射
   private static readonly heroFacetMap: Map<string, number[]> = new Map([
-    ['npc_dota_hero_tinker', [1, 2]],
     ['npc_dota_hero_abaddon', [1, 2]],
     ['npc_dota_hero_axe', [1, 2]],
     ['npc_dota_hero_bane', [1, 2]],
     ['npc_dota_hero_bounty_hunter', [1, 2]],
+    ['npc_dota_hero_bloodseeker', [1, 2]],
     ['npc_dota_hero_bristleback', [2, 3]],
     ['npc_dota_hero_chaos_knight', [2, 3, 4]],
     ['npc_dota_hero_crystal_maiden', [3, 4]],
@@ -19,7 +19,25 @@ export class HeroFacetConfig {
     ['npc_dota_hero_kunkka', [1, 2]],
     ['npc_dota_hero_lich', [1, 2]],
     ['npc_dota_hero_lina', [1, 2]],
-
+    ['npc_dota_hero_lion', [1, 2]],
+    ['npc_dota_hero_luna', [2, 3]],
+    ['npc_dota_hero_medusa', [1, 3, 4]],
+    ['npc_dota_hero_meepo', [0]],
+    ['npc_dota_hero_necrolyte', [1, 2]],
+    ['npc_dota_hero_nevermore', [1, 2]],
+    ['npc_dota_hero_ogre_magi', [1, 2]],
+    ['npc_dota_hero_omniknight', [1, 2]],
+    ['npc_dota_hero_oracle', [1, 2]],
+    ['npc_dota_hero_phantom_assassin', [2, 3]],
+    ['npc_dota_hero_pudge', [1, 3]],
+    ['npc_dota_hero_riki', [1, 2]],
+    ['npc_dota_hero_sand_king', [1, 2]],
+    ['npc_dota_hero_shadow_shaman', [2, 3]],
+    ['npc_dota_hero_skywrath_mage', [1, 2]],
+    ['npc_dota_hero_sniper', [1, 2]],
+    ['npc_dota_hero_spectre', [1, 2]],
+    ['npc_dota_hero_sven', [1, 2]],
+    ['npc_dota_hero_tinker', [1, 2]],
     // ... 其他英雄配置
   ]);
 
