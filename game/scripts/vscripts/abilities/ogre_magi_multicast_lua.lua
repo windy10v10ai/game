891be6597cf2b02c33ad4db839b32e756771b1da
windy <windybirth@gmail.com>
2025-06-20 09:56:52 +0900

    Fix multicast cursor (#1509)
    * Update ogre_magi_multicast_lua.lua
    
    * Update ogre_magi_multicast_lua.lua
    
    * Update lottery-abilities.ts
    
    * fix multicast

--- game/scripts/vscripts/abilities/ogre_magi_multicast_lua.lua
+++ game/scripts/vscripts/abilities/ogre_magi_multicast_lua.lua
@@ -54,35 +54,40 @@ no_support_abilitys = {
 	viper_viper_strike = 1,
 	death_prophet_carrion_swarm = 1,
 	obsidian_destroyer_arcane_orb = 1,
-	ancient_apparition_frost_seal = 1, -- 冰霜封印
-	terrorblade_reflection = 1,       -- 倒影
-	terrorblade_metamorphosis = 1,    -- 魔化
-	vengefulspirit_nether_swap = 1,   -- 移形换位
-	terrorblade_sunder = 1,           -- 魂断
-	pudge_rot = 1,                    -- 腐肉
-	winter_wyvern_arctic_burn = 1,    -- 严寒灼烧
-	kez_switch_weapons = 1,           -- 流派变换 凯
-	faceless_void_time_walk = 1,      -- 时间漫游
-	faceless_void_time_walk_reverse = 1, -- 反时间漫游
-	rubick_spell_steal = 1,           -- 技能窃取
-	magnataur_skewer = 1,             -- 巨角冲撞
-	ember_spirit_sleight_of_fist = 1, -- 无影拳
-	goku_kaioken = 1,                 -- 界王拳
-	doom_bringer_doom = 1,            -- 末日
-	tusk_snowball = 1,                -- 雪球
-	tiny_tree_channel = 1,            -- 树木连掷
-	shredder_chakram = 1,             -- 锯齿飞轮
-	shredder_twisted_chakram = 1,     -- 锯齿飞轮
-	yukari_moon_portal = 1,           -- 八云紫 3技能
-	yukari_twin_trains = 1,           -- 八云紫 大招
-	storm_spirit_ball_lightning = 1,  --  球状闪电
-	phantom_assassin_phantom_strike = 1, --  幻影突袭
-	earthshaker_enchant_totem = 1,    --  强化图腾
-	wisp_tether = 1,                  -- 羁绊
-	dawnbreaker_celestial_hammer = 1, -- 上界重锤
-	nyx_assassin_burrow = 1,          -- 钻地
-	nyx_assassin_unburrow = 1,        -- 现身
-	ancient_apparition_ice_blast = 1, -- 冰晶爆轰
+	ancient_apparition_frost_seal = 1,     -- 冰霜封印
+	terrorblade_reflection = 1,            -- 倒影
+	terrorblade_metamorphosis = 1,         -- 魔化
+	vengefulspirit_nether_swap = 1,        -- 移形换位
+	terrorblade_sunder = 1,                -- 魂断
+	pudge_rot = 1,                         -- 腐肉
+	winter_wyvern_arctic_burn = 1,         -- 严寒灼烧
+	kez_switch_weapons = 1,                -- 流派变换 凯
+	faceless_void_time_walk = 1,           -- 时间漫游
+	faceless_void_time_walk_reverse = 1,   -- 反时间漫游
+	rubick_spell_steal = 1,                -- 技能窃取
+	magnataur_skewer = 1,                  -- 巨角冲撞
+	ember_spirit_sleight_of_fist = 1,      -- 无影拳
+	goku_kaioken = 1,                      -- 界王拳
+	doom_bringer_doom = 1,                 -- 末日
+	tusk_snowball = 1,                     -- 雪球
+	tiny_tree_channel = 1,                 -- 树木连掷
+	shredder_chakram = 1,                  -- 锯齿飞轮
+	shredder_twisted_chakram = 1,          -- 锯齿飞轮
+	yukari_moon_portal = 1,                -- 八云紫 3技能
+	yukari_twin_trains = 1,                -- 八云紫 大招
+	storm_spirit_ball_lightning = 1,       --  球状闪电
+	phantom_assassin_phantom_strike = 1,   --  幻影突袭
+	earthshaker_enchant_totem = 1,         --  强化图腾
+	wisp_tether = 1,                       -- 羁绊
+	dawnbreaker_celestial_hammer = 1,      -- 上界重锤
+	nyx_assassin_burrow = 1,               -- 钻地
+	nyx_assassin_unburrow = 1,             -- 现身
+	ancient_apparition_ice_blast = 1,      -- 冰晶爆轰
+	ancient_apparition_ice_blast_release = 1, -- 冰晶爆轰
+	elder_titan_ancestral_spirit = 1,      -- 灵体游魂
+	primal_beast_onslaught = 1,            -- 獸 突
+	hoodwink_sharpshooter = 1,             -- 一箭穿心
+	hoodwink_sharpshooter_release = 1,     -- 一箭穿心
 }
 no_support_items = {
 	-- 消耗品
@@ -134,16 +139,16 @@ function modifier_ogre_magi_multicast_lua:OnAbilityExecuted(keys)
 	local abilityName = ability:GetName()
 	--不支持技能池
 	if no_support_abilitys[abilityName] then
-		print("no_support_abilitys")
+		print("no_support_abilitys" .. abilityName)
 		return nil
 	end
 	if no_support_items[abilityName] then
-		print("no_support_items")
+		print("no_support_items" .. abilityName)
 		return nil
 	end
 	for _, s in ipairs(no_support_substrings) do
 		if string.find(abilityName, s) then
-			print("no_support_substrings")
+			print("no_support_substrings" .. abilityName)
 			return nil
 		end
 	end
@@ -203,6 +208,9 @@ function modifier_ogre_magi_multicast_lua:OnAbilityExecuted(keys)
 
 	--设置目标再次施法
 	ability:SetContextThink("think_multicast", function()
+		-- 记忆当前cursor position target
+		local cursorPositionBeforeCast = keys.unit:GetCursorPosition()
+
 		if IsHeroUncontrollable(keys.unit) then
 			ability.multicast = nil
 			return nil
@@ -217,10 +225,8 @@ function modifier_ogre_magi_multicast_lua:OnAbilityExecuted(keys)
 		-- 充能技能
 		if ability:GetMaxAbilityCharges(ability:GetLevel()) > 0 then
 			if ability:IsItem() then
-				print("SetCurrentCharges item")
 				ability:SetCurrentCharges(ability:GetCurrentCharges() + 1)
 			else
-				print("SetCurrentAbilityCharges")
 				ability:SetCurrentAbilityCharges(ability:GetCurrentAbilityCharges() + 1)
 			end
 		end
@@ -238,12 +244,16 @@ function modifier_ogre_magi_multicast_lua:OnAbilityExecuted(keys)
 			end
 		end
 
+		-- 施法
 		ability:EndCooldown()
 		keys.unit:SetCursorCastTarget(target)
 		keys.unit:SetCursorPosition(pos)
 		keys.unit:CastAbilityImmediately(ability, 0)
+		-- 施法结束后
 		-- 返还魔法
 		keys.unit:GiveMana(ability:GetManaCost(-1))
+		-- 还原cursor position target
+		keys.unit:SetCursorPosition(cursorPositionBeforeCast)
 
 		ability.multicast = ability.multicast - 1
 		if ability.multicast <= 1 then
