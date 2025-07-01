52f279ce60372d77edb1de6cc6c7bab4332e9796
windy <windybirth@gmail.com>
2025-07-01 20:43:17 +0900

    Revert "test ts error"
    This reverts commit b7655087b4e76c52540c2409194d3017074daa82.

--- src/vscripts/modules/event/game-end/game-end.ts
+++ src/vscripts/modules/event/game-end/game-end.ts
@@ -51,7 +51,7 @@ export class GameEnd {
 
     const players: GameEndPlayerDto[] = [];
     PlayerHelper.ForEachPlayer((playerId) => {
-      const player = PlayerResource.GetPlaye(playerId);
+      const player = PlayerResource.GetPlayer(playerId);
       if (!player) {
         return;
       }
