9750645d28182e5ed72766a932b17c5c73b132b7
Jinlong-zju <61981216+Jinlong-zju@users.noreply.github.com>
2025-11-09 01:08:26 +0800

    魔渊炸房

--- game/scripts/vscripts/items/item_magic_crit_blade.lua
+++ game/scripts/vscripts/items/item_magic_crit_blade.lua
@@ -163,7 +163,7 @@ function modifier_item_magic_crit_blade:OnAttackLanded(params)
         victim = params.target,
         attacker = params.attacker,
         damage = damage,
-        damage_type = DAMAGE_TYPE_MAGICAL + DOTA_DAMAGE_FLAG_REFLECTION,
+        damage_type = DAMAGE_TYPE_MAGICAL,
         ability = self:GetAbility(),
     })
     -- 在下一帧清除标记
@@ -174,7 +174,7 @@ function modifier_item_magic_crit_blade:OnAttackLanded(params)
     EmitSoundOn("Hero_Brewmaster.Brawler.Crit", params.target)
     -- 显示幻影暴击伤害数字(蓝紫色)
     local total_spell_amp = self:GetParent():GetSpellAmplification(false)
-    SendOverheadEventMessage(nil, OVERHEAD_ALERT_DAMAGE, params.target, damage * (total_spell_amp + 1), nil)
+    SendOverheadEventMessage(nil, OVERHEAD_ALERT_BONUS_SPELL_DAMAGE, params.target, damage * (total_spell_amp + 1), nil)
 end
 
 function modifier_item_magic_crit_blade:GetModifierSpellAmplify_Percentage()
@@ -307,7 +307,7 @@ function modifier_item_magic_crit_blade:OnTakeDamage(params)
             --print(string.format("[MagicCritBlade] 必然暴击进入冷却: %.0f秒", cooldown))
         end
         -- 使用CRITICAL伤害类型显示更大的数字
-        SendOverheadEventMessage(nil, OVERHEAD_ALERT_DAMAGE, target, extra_damage, nil)
+        SendOverheadEventMessage(nil, OVERHEAD_ALERT_BONUS_SPELL_DAMAGE, target, extra_damage, nil)
 
         -- 【修改】更改音效为更震撼的暴击音效
         EmitSoundOn("Hero_PhantomAssassin.CoupDeGrace", target) -- PA大招音效
