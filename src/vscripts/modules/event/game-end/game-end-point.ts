1cb4af4c0643db88453cde91985db91534fef2b3
windy <windybirth@gmail.com>
2025-11-11 12:51:37 +0900

    Hotfix bots (#1737)
    * Hotfix bots
    
    * Update GameConfig.ts
    
    * fix bots
    
    * 积分暂时减半
    
    Former-commit-id: e58e109affb6885d5be9a6d3149daaf895f625ea

--- src/vscripts/modules/event/game-end/game-end-point.ts
+++ src/vscripts/modules/event/game-end/game-end-point.ts
@@ -57,28 +57,31 @@ export class GameEndPoint {
 
   static GetDifficultyMultiplier(difficulty: number, isLocalhost: boolean, option: Option): number {
     // 如果是作弊模式，不计算倍率。开发模式无视这条
-    if (!IsInToolsMode()) {
-      if (GameRules.IsCheatMode() || isLocalhost) {
-        return 0;
-      }
-    }
+    // TODO bots只有作弊模式生效，暂时给分，等后面再修改
+    // if (!IsInToolsMode()) {
+    //   if (GameRules.IsCheatMode() || isLocalhost) {
+    //     return 0;
+    //   }
+    // }
+
+    const baseMultiplier = 0.5;
 
     switch (difficulty) {
       case 1:
-        return 1.2;
+        return baseMultiplier * 1.2;
       case 2:
-        return 1.4;
+        return baseMultiplier * 1.4;
       case 3:
-        return 1.6;
+        return baseMultiplier * 1.6;
       case 4:
-        return 1.8;
+        return baseMultiplier * 1.8;
       case 5:
-        return 2;
+        return baseMultiplier * 2;
       case 6:
-        return 2.2;
+        return baseMultiplier * 2.2;
       default:
         // 自定义模式
-        return this.GetCustomModeMultiplier(option);
+        return baseMultiplier * this.GetCustomModeMultiplier(option);
     }
   }
 
