4eacf67c72db72408a8d77b1b0a583ea2af1d29c
windy <windybirth@gmail.com>
2025-12-20 16:50:03 +0900

    treant

--- src/vscripts/modules/lottery/lottery-abilities.ts
+++ src/vscripts/modules/lottery/lottery-abilities.ts
@@ -90,6 +90,7 @@ export const abilityTiersActive: Tier[] = [
       'kunkka_ghostship', // 幽灵船
       'pangolier_gyroshell', // 地雷滚滚
       'void_spirit_astral_step', // 太虚之径
+      'treant_overgrowth', // 疯狂生长
 
       // 小技能
       'lion_impale', // 裂地尖刺
@@ -111,6 +112,8 @@ export const abilityTiersActive: Tier[] = [
       'antimage_blink', // 闪烁
       'ringmaster_tame_the_beasts', // 驯兽术
       'ringmaster_impalement', // 尖刀戏
+      'slark_saltwater_shiv', // 海浪短刀
+      'treant_natures_grasp', // 自然卷握
 
       // 自定义技能
       'lina_flame_cloak2', // 火女 腾焰斗篷
