62793818686d08c3df5a0ba4990782e94db21a17
windy <windybirth@gmail.com>
2026-01-18 16:33:37 +0900

    setting

--- src/vscripts/modules/GameConfig.ts
+++ src/vscripts/modules/GameConfig.ts
@@ -78,7 +78,7 @@ export class GameConfig {
     if (IsInToolsMode()) {
       print('[GameConfig] 开发者模式快速开始游戏');
       GameRules.SetCustomGameSetupAutoLaunchDelay(5);
-      // GameRules.SetHeroSelectionTime(5);
+      GameRules.SetHeroSelectionTime(5);
       GameRules.SetHeroSelectPenaltyTime(1); // 选择英雄超时惩罚时间
       GameRules.SetStrategyTime(3);
       GameRules.SetPreGameTime(5); // 进入游戏后号角吹响前的准备时间
