4495e902e5987d615c8237de41446df85d3b90cd
windy <windybirth@gmail.com>
2026-04-29 01:06:26 +0900

    Update StatsTab.tsx

--- src/panorama/react/hud_main/pages/profile/tabs/StatsTab.tsx
+++ src/panorama/react/hud_main/pages/profile/tabs/StatsTab.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useNetTable } from '../../../../shared/hooks/useNetTable';
+import { GetLocalPlayerSteamAccountID } from '@utils/utils';
+
+const AVATAR_BORDER_GOLD =
+  'url("s2r://panorama/images/custom_game/profile/avatar-square-gold-border.png")';
+const AVATAR_BORDER_NORMAL =
+  'url("s2r://panorama/images/custom_game/profile/avatar-square-normal-border.png")';
 
 /**
  * 战绩 Tab：左侧头像/昵称面板 + 右侧数据卡片。
+ * 头像边框根据 member_table.enable 切换 gold / normal。
  */
 export function StatsTab() {
+  const steamId = GetLocalPlayerSteamAccountID();
+  const member = useNetTable('member_table', steamId);
+  const isMember = member != null && (member.enable as unknown as number) === 1;
+  const borderUrl = isMember ? AVATAR_BORDER_GOLD : AVATAR_BORDER_NORMAL;
+
   return (
     <Panel className="stats-layout">
       <Panel className="stats-player-card">
-        <Panel className="stats-avatar-wrap">
+        <Panel className="stats-avatar-wrap" style={{ backgroundImage: borderUrl }}>
           <DOTAAvatarImage
             className="stats-avatar"
             steamid="local"
