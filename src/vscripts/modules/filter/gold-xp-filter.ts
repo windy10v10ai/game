a225b8733a2317c995c10781d81b80327de56113
windy <windybirth@gmail.com>
2026-05-10 21:53:39 +0900

    balance

--- src/vscripts/modules/filter/gold-xp-filter.ts
+++ src/vscripts/modules/filter/gold-xp-filter.ts
@@ -3,8 +3,8 @@ import { PlayerHelper } from '../helper/player-helper';
 
 @reloadable
 export class GoldXPFilter {
-  private readonly REDUCE_RATE_3_MIN = 0.5;
-  private readonly REDUCE_RATE_6_MIN = 0.75;
+  private readonly REDUCE_RATE_3_MIN = 0.6;
+  private readonly REDUCE_RATE_6_MIN = 0.8;
   private readonly REDUCE_RATE_HERO_KILL = 0.5;
 
   constructor() {
