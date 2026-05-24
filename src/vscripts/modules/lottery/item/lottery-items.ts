fd954a165a0805ebaeb42c1bd551feabed6eaa37
windy <windybirth@gmail.com>
2026-05-24 18:38:09 +0900

    items

--- src/vscripts/modules/lottery/item/lottery-items.ts
+++ src/vscripts/modules/lottery/item/lottery-items.ts
@@ -5,7 +5,7 @@ import { Tier } from '../shared/tier';
  * 单格出现概率：T5 1% / T4 4% / T3 15% / T2 40% / T1 40%
  */
 export const itemTiers: Tier[] = [
-  // 20k
+  // 终极物品
   {
     level: 5,
     names: [
@@ -23,7 +23,7 @@ export const itemTiers: Tier[] = [
       'item_tome_of_strength', // 力量之书
     ],
   },
-  // 5k
+  // 特殊物品
   {
     level: 4,
     names: [
@@ -33,33 +33,40 @@ export const itemTiers: Tier[] = [
       'item_consumable_gem', // 幻影宝石
       'item_rune_transmuter_advanced', // 转化石
       'item_candy_candy', // 嘉心糖
+      'item_meteor_hammer_2', // 星落
     ],
   },
-  // 3k
+  // 3~5k
   {
     level: 3,
     names: [
-      'item_hand_of_midas', // 点金手
-      'item_holy_locket', // 圣洁吊坠
+      'item_tome_of_knowledge', // 知识之书
       'item_aghanims_shard', // 阿哈利姆魔晶
-      'item_great_famango', // 大疗伤莲花
+      'item_solar_crest', // 炎阳纹章
       'item_aether_lens_2', // 以太之镜
-      'item_glimmer_cape', // 微光披风
-      'item_force_staff', // 原力法杖
+      'item_wings_of_haste', // 急速之翼
+      'item_crellas_crozier', // 克莱拉牧杖
+      'item_specialists_array', // 行家阵列
+      'item_hand_of_group', // 团队之手
     ],
   },
-  // 2k
+  // 1~2k
   {
     level: 2,
     names: [
-      'item_tome_of_knowledge', // 知识之书
-      'item_bottle', // 魔瓶
+      'item_great_famango', // 大疗伤莲花
       'item_arcane_boots', // 秘法鞋
       'item_phase_boots', // 相位鞋
       'item_power_treads', // 动力鞋
+      'item_hand_of_midas', // 点金手
+      'item_force_staff', // 原力法杖
+      'item_glimmer_cape', // 微光披风
+      'item_essence_distiller', // 精之灵器
+      'item_ancient_janggo', // 韧鼓
+      'item_holy_locket', // 圣洁吊坠
     ],
   },
-  // 1k
+  // <1k
   {
     level: 1,
     names: [
@@ -68,6 +75,7 @@ export const itemTiers: Tier[] = [
       'item_null_talisman', // 挂件
       'item_infused_raindrop', // 凝魂之露
       'item_magic_wand', // 魔棒
+      'item_bottle', // 魔瓶
     ],
   },
 ];
