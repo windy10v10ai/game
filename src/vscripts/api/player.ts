bc2fbebcb09122890564215fcfe98977d520d765
windy <windybirth@gmail.com>
2026-05-31 15:50:13 +0900

    fix

--- src/vscripts/api/player.ts
+++ src/vscripts/api/player.ts
@@ -35,11 +35,13 @@ export class PlayerInfoDto {
 
   seasonPointTotal!: number;
   seasonLevel!: number;
+  seasonCurrrentLevelPoint!: number;
   seasonNextLevelPoint!: number;
   useableSeasonPoint!: number;
 
   memberPointTotal!: number;
   memberLevel!: number;
+  memberCurrentLevelPoint!: number;
   memberNextLevelPoint!: number;
   useableMemberPoint!: number;
 
