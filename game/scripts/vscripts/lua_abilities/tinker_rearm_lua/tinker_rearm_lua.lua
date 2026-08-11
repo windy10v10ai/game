3247c9f8c83c76feed1bd9bcd27482cac942e566
windy <windybirth@gmail.com>
2026-08-11 14:44:20 +0900

    balance

--- game/scripts/vscripts/lua_abilities/tinker_rearm_lua/tinker_rearm_lua.lua
+++ game/scripts/vscripts/lua_abilities/tinker_rearm_lua/tinker_rearm_lua.lua
@@ -135,8 +135,9 @@ tinker_rearm_lua.ItemException = {
 	["item_candy_candy"] = true,
 	["item_switchable_crit_blade"] = true,
 	["item_magic_crit_blade"] = true,
-	["item_time_gem"] = true,  -- 时间宝石
+	["item_time_gem"] = true,     -- 时间宝石
 	["item_beast_shield"] = true, -- 兽化盾
+	["item_withered_spring"] = true, -- 生命之心
 }
 
 --------------------------------------------------------------------------------
